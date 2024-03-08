04db40e83890eac44ead12f334ec22a748cc9963
Waleed Waheed <waleed.waheed@globalnorthstar.com>
2024-03-09 00:11:40 +0500

    Waleed

--- src/pages/Listing.js
+++ src/pages/Listing.js
@@ -1,14 +1,4 @@
 import * as React from "react";
-import Box from "@mui/material/Box";
-import Grid from "@mui/material/Grid";
-// import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
-import Link from "@mui/material/Link";
-import Typography from "@mui/material/Typography";
-import Chip from "@mui/material/Chip";
 import roomImg from "../images/room-img.png";
 import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
 import BedOutlinedIcon from '@mui/icons-material/BedOutlined';
