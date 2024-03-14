e3d25c57aec4114c84d21ee6e809793a7144b667
Waleed Waheed <waleed.waheed@globalnorthstar.com>
2024-03-14 16:53:25 +0500

    Waleed

---
+++ src/ui/ListingCarousel.js
@@ -0,0 +1,39 @@
+import React, { useRef, useEffect, PropsWithChildren } from 'react';
+
+import { Carousel as NativeCarousel } from '@fancyapps/ui';
+import '@fancyapps/ui/dist/carousel/carousel.css';
+
+import { Thumbs } from '@fancyapps/ui/dist/carousel/carousel.thumbs.esm.js';
+import '@fancyapps/ui/dist/carousel/carousel.thumbs.css';
+
+const defaults = {
+  Dots: false,
+  Thumbs: false,
+  transition: "slide"
+};
+
+function Carousel(props) {
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    const container = containerRef.current;
+    const options = {
+      ...defaults,
+      ...(props.options || {}),
+    };
+
+    const instance = new NativeCarousel(container, options, { Thumbs });
+
+    return () => {
+      instance.destroy();
+    };
+  });
+
+  return (
+    <div className="f-carousel" ref={containerRef}>
+      {props.children}
+    </div>
+  );
+}
+
+export default Carousel;
